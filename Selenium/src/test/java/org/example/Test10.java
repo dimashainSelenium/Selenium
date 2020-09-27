d1f002e42dc268404c059b198c59cff044b0fae9
dimashainSelenium <70927102+dimashainSelenium@users.noreply.github.com>
2020-09-28 00:23:50 +0300

    Update Test10.java

--- Selenium/src/test/java/org/example/Test10.java
+++ Selenium/src/test/java/org/example/Test10.java
@@ -33,20 +33,22 @@ public class Test10 {
         discountPrice1 = new String[5];
 
         regularPrice1[0] = productUnit.findElement(By.cssSelector(".regular-price")).getText();
-        regularPrice1[1]= productUnit.findElement(By.cssSelector(".regular-price")).getCssValue("color");
-        regularPrice1[2]= productUnit.findElement(By.cssSelector(".regular-price")).getCssValue("font-weight");
-        regularPrice1[3]= productUnit.findElement(By.cssSelector(".regular-price")).getCssValue("text-decoration");
-        regularPrice1[4]= productUnit.findElement(By.cssSelector(".regular-price")).getCssValue("font-size");
+        regularPrice1[1]= productUnit.findElement(By.cssSelector(".regular-price")).getCssValue("font-weight");
+        regularPrice1[2]= productUnit.findElement(By.cssSelector(".regular-price")).getCssValue("text-decoration");
+        regularPrice1[3]= productUnit.findElement(By.cssSelector(".regular-price")).getCssValue("font-size");
+        regularPrice1[4]= productUnit.findElement(By.cssSelector(".regular-price")).getCssValue("color");
+        greyCheck(regularPrice1[4]);
 
-        size1 = Float.parseFloat(regularPrice1[4].replaceAll("px",""));
+        size1 = Float.parseFloat(regularPrice1[3].replaceAll("px",""));
 
         discountPrice1[0]= productUnit.findElement(By.cssSelector(".campaign-price")).getText();
-        discountPrice1[1]= productUnit.findElement(By.cssSelector(".campaign-price")).getCssValue("color");
-        discountPrice1[2]= productUnit.findElement(By.cssSelector(".campaign-price")).getCssValue("font-weight");
-        discountPrice1[3]= productUnit.findElement(By.cssSelector(".campaign-price")).getCssValue("text-decoration");
-        discountPrice1[4]= productUnit.findElement(By.cssSelector(".campaign-price")).getCssValue("font-size");
+        discountPrice1[1]= productUnit.findElement(By.cssSelector(".campaign-price")).getCssValue("font-weight");
+        discountPrice1[2]= productUnit.findElement(By.cssSelector(".campaign-price")).getCssValue("text-decoration");
+        discountPrice1[3]= productUnit.findElement(By.cssSelector(".campaign-price")).getCssValue("font-size");
+        discountPrice1[4]= productUnit.findElement(By.cssSelector(".campaign-price")).getCssValue("color");
+        redCheck(discountPrice1[4]);
 
-        size2=Float.parseFloat(discountPrice1[4].replaceAll("px",""));
+        size2=Float.parseFloat(discountPrice1[3].replaceAll("px",""));
 
         Assert.assertTrue(size1<size2);
 
@@ -63,24 +65,26 @@ public class Test10 {
         discountPrice2 = new String[5];
 
         regularPrice2[0]= productUnit.findElement(By.cssSelector(".regular-price")).getText();
-        regularPrice2[1]= productUnit.findElement(By.cssSelector(".regular-price")).getCssValue("color");
-        regularPrice2[2]= productUnit.findElement(By.cssSelector(".regular-price")).getCssValue("font-weight");
-        regularPrice2[3]= productUnit.findElement(By.cssSelector(".regular-price")).getCssValue("text-decoration");
-        regularPrice2[4]= productUnit.findElement(By.cssSelector(".regular-price")).getCssValue("font-size");
+        regularPrice2[1]= productUnit.findElement(By.cssSelector(".regular-price")).getCssValue("font-weight");
+        regularPrice2[2]= productUnit.findElement(By.cssSelector(".regular-price")).getCssValue("text-decoration");
+        regularPrice2[3]= productUnit.findElement(By.cssSelector(".regular-price")).getCssValue("font-size");
+        regularPrice2[4]= productUnit.findElement(By.cssSelector(".regular-price")).getCssValue("color");
+        greyCheck(regularPrice2[4]);
 
-        size1 = Float.parseFloat(regularPrice2[4].replaceAll("px",""));
+        size1 = Float.parseFloat(regularPrice2[3].replaceAll("px",""));
 
         discountPrice2[0]= productUnit.findElement(By.cssSelector(".campaign-price")).getText();
-        discountPrice2[1]= productUnit.findElement(By.cssSelector(".campaign-price")).getCssValue("color");
-        discountPrice2[2]= productUnit.findElement(By.cssSelector(".campaign-price")).getCssValue("font-weight");
-        discountPrice2[3]= productUnit.findElement(By.cssSelector(".campaign-price")).getCssValue("text-decoration");
-        discountPrice2[4]= productUnit.findElement(By.cssSelector(".campaign-price")).getCssValue("font-size");
+        discountPrice2[1]= productUnit.findElement(By.cssSelector(".campaign-price")).getCssValue("font-weight");
+        discountPrice2[2]= productUnit.findElement(By.cssSelector(".campaign-price")).getCssValue("text-decoration");
+        discountPrice2[3]= productUnit.findElement(By.cssSelector(".campaign-price")).getCssValue("font-size");
+        discountPrice2[4]= productUnit.findElement(By.cssSelector(".campaign-price")).getCssValue("color");
+        redCheck(discountPrice2[4]);
 
-        size2=Float.parseFloat(discountPrice2[4].replaceAll("px",""));
+        size2=Float.parseFloat(discountPrice2[3].replaceAll("px",""));
 
         Assert.assertTrue(size1<size2);
 
-        for(int i=0; i<5;i++) {
+        for(int i=0; i<4;i++) {
             Assert.assertTrue(regularPrice1[i].compareTo(regularPrice2[i])==0);
             Assert.assertTrue(discountPrice1[i].compareTo(discountPrice2[i])==0);
         }
@@ -88,5 +92,58 @@ public class Test10 {
         Assert.assertTrue(size1<size2);
         TimeUnit.SECONDS.sleep(1);
         driver.quit();
+    }
+
+    public void redCheck(String color)
+    {
+        String[] rgb;
+        char c = 0;
+        int j = 0;
+        rgb = new String[3];
+
+        color = color.substring(5);
+        color = color.substring(0,color.length()-4);
+        for(int i=0; i<color.length();i++) {
+            if (c==','){
+                j = j+1;
+                c=0;
+            }
+            else {
+            c = color.charAt(i);
+            rgb[j]=rgb[j] + (String.valueOf(c));
+            }
+        }
+        rgb[0] = rgb[0].replaceAll("null","").replace(',',' ').trim();
+        rgb[1] = rgb[1].replaceAll("null","").replace(',',' ').trim();
+        rgb[2] = rgb[2].replaceAll("null","").replace(',',' ').trim();
+
+        Assert.assertTrue(Integer.parseInt(rgb[0])>Integer.parseInt(rgb[1]));
+    }
+
+    public void greyCheck(String color)
+    {
+        String[] rgb;
+        char c = 0;
+        int j = 0;
+        rgb = new String[3];
+
+        color = color.substring(5);
+        color = color.substring(0,color.length()-4);
+        for(int i=0; i<color.length();i++) {
+            if (c==','){
+                j = j+1;
+                c=0;
+            }
+            else {
+                c = color.charAt(i);
+                rgb[j]=rgb[j] + (String.valueOf(c));
+            }
+        }
+        rgb[0] = rgb[0].replaceAll("null","").replace(',',' ').trim();
+        rgb[1] = rgb[1].replaceAll("null","").replace(',',' ').trim();
+        rgb[2] = rgb[2].replaceAll("null","").replace(',',' ').trim();
+
+        Assert.assertTrue(Integer.parseInt(rgb[0]) == Integer.parseInt(rgb[1]));
+        Assert.assertTrue(Integer.parseInt(rgb[1]) == Integer.parseInt(rgb[2]));
     }
 }
